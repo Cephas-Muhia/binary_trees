9effbaf8e1ce4a21415c7b7b14ef61dd3f31f636
Cephas-Muhia <cephaschronixx@gmail.com>
2024-03-27 09:53:06 -0700

    Nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,28 +1,23 @@
 #include "binary_trees.h"
 /**
- * bst_search - search for a special value in the tree and return the node
- * @tree: tree to go through
- * @value: value to search
- * Return: the node with the value or NULL if the value is not in the tree
+ * binary_tree_nodes - function that returns # of nodes in a tree with children
+ * @tree: tree to check
+ * Return: number of nodes with children
  */
-bst_t *bst_search(const bst_t *tree, int value)
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	bst_t *found;
+
+	size_t  node = 0;
 
 	if (tree == NULL)
-		return (NULL);
-
-	if (value < tree->n)
 	{
-		found = bst_search(tree->left, value);
+		return (0);
 	}
-	else if (value > tree->n)
-	{
-		found = bst_search(tree->right, value);
-	}
-	else if (value == tree->n)
-		return ((bst_t *)tree);
 	else
-		return (NULL);
-	return (found);
+	{
+		node += ((tree->left || tree->right) ? 1 : 0);
+		node += binary_tree_nodes(tree->left);
+		node += binary_tree_nodes(tree->right);
+		return (node);
+	}
 }
